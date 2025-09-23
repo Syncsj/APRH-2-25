a2987a1b38c4ff7c7c08a9a7abf06f13432d07f1
Syncsj <marcbucher@tsn.at>
2025-09-23 09:02:42 +0200

    Hallo

---
+++ src/k5/Medium/CD.java
@@ -0,0 +1,17 @@
+package k5.Medium;
+
+public class CD {
+    //interpreter
+    //anzahl an titeln
+    String interpreter;
+    int titel;
+
+    //get Anzahl
+
+
+
+    //abspiel (interpret + titelanzahl ausgeben)
+    public void anzahl(){
+        System.out.println(interpreter + titel);
+    }
+}
---
+++ src/k5/Medium/Radio.java
@@ -0,0 +1,12 @@
+package k5.Medium;
+
+public class Radio {
+    public static void main(String[] args) {
+
+        CD cd1 = new CD();
+
+        cd1.interpreter = "Leser";
+        cd1.titel = 15;
+        cd1.anzahl();
+    }
+}
